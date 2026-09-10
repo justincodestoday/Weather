66607577264ba5a9cab508e7db2bf73613b57cd6
Justin Chong <103158251+justincodestoday@users.noreply.github.com>
2026-09-10 18:15:47 +0800

    Updates to files

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,5 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-  ],
+  plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
 });
