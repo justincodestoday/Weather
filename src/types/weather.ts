b2ebdabc1bcf2b1895d3e5c001f4b3ee4609ccb7
Justin Chong <103158251+justincodestoday@users.noreply.github.com>
2026-09-10 23:04:32 +0800

    Define weather data response model and history entry model

---
+++ src/types/weather.ts
@@ -0,0 +1,20 @@
+export interface WeatherDataResponse {
+  main: {
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+    humidity: number;
+  };
+  weather: { id: number; main: string }[];
+  dt: number;
+  timezone: number;
+  sys: { country: string };
+  name: string;
+}
+
+export interface HistoryEntry {
+  id: string;
+  city: string;
+  countryCode: string;
+  searchedAt: number;
+}
