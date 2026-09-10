4def2fc580a1370ff8e1de1919df5531689a5eae
Justin Chong <103158251+justincodestoday@users.noreply.github.com>
2026-09-11 01:52:12 +0800

    Install test libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), babel({ presets: [reactCompilerPreset()] })],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    env: { VITE_OPENWEATHER_API_KEY: "test-key" },
+  },
 });
